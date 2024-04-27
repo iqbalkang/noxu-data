1f2ebf833661d4820928b1b848d415f795bb3351
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2024-04-26 22:45:01 -0700

    removed cn.ts file which was empty

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Answer from '@/components/Answer';
 import Chat from '@/components/Chat';
 import Question from '@/components/Question';
-import Image from 'next/image';
 
 export default function Home() {
   return (
